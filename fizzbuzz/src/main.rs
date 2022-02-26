1c03e8443fb0fb34ed01e55b1358013728d2f187
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2022-02-25 23:30:14 -0800

    init commit

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Welcome to FizzBuzz in Rust");
+    println!("Enter n: ");
+
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+
+    let n: u32 = loop {
+        let value: i32 = line.trim().parse().unwrap();
+        if value >= 0 {
+            break value as u32
+        }
+    };
+    println!("FizzBuzz...");
+    fizzbuzz(n);
+}
+
+fn fizzbuzz(n: u32) {
+    for i in 1..n+1 {
+        if i % 3 == 0 || i % 5 == 0 {
+            if i % 3 == 0 {
+                print!("Fizz");
+            }
+            if i % 5 == 0 {
+                print!("Buzz");
+            }
+        } else {
+            print!("{}", i);
+        }
+        println!();
+    }
+}
